070b83771d46e135cb3059f3b120a9d000bbc555
vivekm92 <viv.mttl@gmail.com>
2021-02-28 22:54:24 +0530

    add repo structure

---
+++ arrays/max-sum-contiguous-subarray.go
@@ -0,0 +1,40 @@
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+
+func maxSubArray(A []int ) (int) {
+
+
+	currSum := math.Inf(-1)
+	res := math.Inf(-1)
+
+	for i := 0; i < len(A); i++ {
+		currVal := float64(A[i])
+
+		if currSum + currVal < currVal {
+			currSum = currVal
+		} else {
+			currSum += currVal
+		}
+
+		if res < currSum {
+			res = currSum
+		}
+	}
+
+	return int(res)
+}
+
+func main() {
+
+	A := []int{1, 2, 3, 4, -10}
+	fmt.Println(maxSubArray(A))
+
+	B := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
+	fmt.Println(maxSubArray(B))
+}
---
+++ arrays/min-steps-in-infinite-grid.go
@@ -0,0 +1,42 @@
+
+package main
+
+import (
+	"fmt"
+)
+
+
+func coverPoints(A []int , B []int )  (int) {
+    
+    res := 0
+    for i := 1; i < len(A); i++ {
+    	xDiff := A[i] - A[i - 1]
+    	yDiff := B[i] - B[i - 1]
+    	if xDiff < 0 {
+    		xDiff = xDiff * -1
+    	}
+    	if yDiff < 0 {
+    		 yDiff = yDiff * -1
+    	}
+
+        if xDiff > yDiff {
+        	res += xDiff
+        } else {
+        	res += yDiff
+        }
+
+    }
+    
+    return res
+}
+
+ 
+func main() {
+
+	A := []int{0, 1, 1}
+	B := []int{0, 1, 2}
+
+
+	fmt.Println(coverPoints(A, B))
+
+}
