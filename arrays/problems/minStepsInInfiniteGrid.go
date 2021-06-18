02bb71823f62f23b2783c592651f7be2b53032bc
vivek mittal <viv.mttl@gmail.com>
2021-06-18 10:05:49 +0530

    update minStepsInInfiniteGrid.go

--- arrays/problems/minStepsInInfiniteGrid.go
+++ arrays/problems/minStepsInInfiniteGrid.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 // T(n) : O(n), S(n) : O(1)
@@ -10,26 +11,14 @@ func coverPoints(A []int , B []int )  (int) {
     
     res := 0
     for i := 1; i < len(A); i++ {
-    	xDiff := A[i] - A[i - 1]
-    	yDiff := B[i] - B[i - 1]
-    	if xDiff < 0 {
-    		xDiff = xDiff * -1
-    	}
-    	if yDiff < 0 {
-    		 yDiff = yDiff * -1
-    	}
-
-        if xDiff > yDiff {
-        	res += xDiff
-        } else {
-        	res += yDiff
-        }
-
+        xDiff := A[i] - A[i - 1]
+        yDiff := B[i] - B[i - 1]
+        
+        res += int(math.Max(math.Abs(float64(xDiff)), math.Abs(float64(yDiff))))
     }
     
     return res
 }
-
  
 func main() {
 
