72a77aa101df3ec3d3e5556c6a9bd60c8ef1bee8
vivek mittal <viv.mttl@gmail.com>
2021-06-20 22:32:49 +0530

    Add maxMin.go

---
+++ arrays/problems/maxMin.go
@@ -0,0 +1,15 @@
+func solve(A []int )  (int) {
+
+    n := len(A)
+    maxElement, minElement := A[0], A[0]
+    for i := 1; i < n; i++ {
+        if A[i] > maxElement {
+            maxElement = A[i]
+        }
+        if A[i] < minElement {
+            minElement = A[i]
+        }
+    }
+
+    return maxElement + minElement
+}
