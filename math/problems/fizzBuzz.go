f6529cb869e03192fac94b53165b9b92d18cb2c0
vivek mittal <viv.mttl@gmail.com>
2021-06-21 11:49:56 +0530

    Add math/fizzBuzz.go

---
+++ math/problems/fizzBuzz.go
@@ -0,0 +1,25 @@
+import "strconv"
+
+// T(n) : O(n), S(n) : O(n)
+func fizzBuzz(A int )  ([]string) {
+
+    res := make([]string, 0)
+    for i := 1; i <= A; i++ {
+        var foo string = ""
+        if i % 3 == 0 {
+            foo += "Fizz"
+        } 
+        
+        if i % 5 == 0 {
+            foo += "Buzz"
+        }
+        
+        if foo == "" {
+            foo = strconv.Itoa(i)
+        }
+
+        res = append(res, foo)
+    }
+
+    return res
+}
