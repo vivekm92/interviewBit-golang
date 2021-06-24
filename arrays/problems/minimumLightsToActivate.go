7464a7cc0651df383322c70f3f3f28c3046c7c22
vivek mittal <viv.mttl@gmail.com>
2021-06-24 10:16:39 +0530

    Add arrays/minimumLightsToActivate.go

---
+++ arrays/problems/minimumLightsToActivate.go
@@ -0,0 +1,34 @@
+
+// T(n) : O(n), S(n) : O(1)
+func solve(A []int , B int )  (int) {
+
+    n, curr, res := len(A), 0, 0
+    for curr < n {
+        next, prev := curr + B - 1, curr - B + 1
+        if prev < 0 { 
+            prev = 0
+        }
+
+        if next >= n {
+            next = n - 1
+        }
+
+        idx := next
+        for idx >= prev {
+            if A[idx] == 1 {
+                break
+            }
+            idx--
+        }
+
+        if idx >= prev {
+            res++
+            curr = idx + B
+        } else {
+            return -1
+        }
+
+    }
+
+    return res
+}
