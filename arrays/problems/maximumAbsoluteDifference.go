83e66ba315003ab43b7239e3a2a87de2d25f96c4
vivek mittal <viv.mttl@gmail.com>
2021-06-24 16:19:59 +0530

    update arrays/maximumAbsoluteDifference.go

--- arrays/problems/maximumAbsoluteDifference.go
+++ arrays/problems/maximumAbsoluteDifference.go
@@ -1,38 +1,29 @@
 
 // T(n): O(n), S(n) : O(1)
-func getMinMax(A []int) (int, int) {
-
-    n := len(A)
-    max, min := -1 << 63, 1 << 63 - 1
-    for i := 0; i < n; i++ {
-        if max < A[i] {
-            max = A[i]
-        }
-        if min > A[i] {
-            min = A[i]
-        }
-    }
-
-    return max, min
-}
-
-// T(n) : O(n), S(n) : O(n)
 func maxArr(A []int )  (int) {
 
-    n, a1, a2 := len(A), make([]int, 0), make([]int, 0)
-    for i := 0; i < n; i++ {
-        a1 = append(a1, A[i] + i)
-    }
+    n, max1, min1, max2, min2 := len(A), -1 << 63, 1 << 63 - 1, -1 << 63, 1 << 63 - 1
 
     for i := 0; i < n; i++ {
-        a2 = append(a2, A[i] - i)
-    }
+        if max1 < A[i] + i {
+            max1 = A[i] + i 
+        }
 
-    max1, min1 := getMinMax(a1)
-    max2, min2 := getMinMax(a2)
+        if min1 > A[i] + i {
+            min1 = A[i] + i
+        }
+
+        if max2 < A[i] - i {
+            max2 = A[i] - i
+        }
+
+        if min2 > A[i] - i {
+            min2 = A[i] - i
+        }
+    }
 
     res := 0
-    if max1 - min1  > max2 - min2 {
+    if max1 - min1 > max2 - min2 {
         res = max1 - min1
     } else {
         res = max2 - min2
