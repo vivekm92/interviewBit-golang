0e0a90e7871a9f44da70610bbacca4cea752b9d3
vivek mittal <viv.mttl@gmail.com>
2021-07-12 12:42:31 +0530

    add maximumSumTriplet.go

---
+++ arrays/problems/maximumSumTriplet.go
@@ -0,0 +1,35 @@
+
+// T(n) : O(n), S(n) : O(1)
+func max(a, b int) (int) { 
+    if a < b {
+        return b
+    }
+    return a
+}
+
+// T(n) : O(n2), S(n) : O(1)
+func solve(A []int )  (int) {
+
+    n, res := len(A), 0
+    for i := 1; i < n - 1; i++ {
+
+        max1, max2 := -1 << 31, -1 << 31
+        for j := i - 1; j >= 0; j-- {
+            if A[j] < A[i] { 
+                max1 = max(max1, A[j]);
+            }
+        }
+
+        for j := i + 1; j < n; j++ {
+            if (A[j] > A[i]) {
+                max2 = max(max2, A[j]);
+            }
+        }
+
+        if max1 != -1 << 31 && max2 != -1 << 31 {
+            res = max(res, A[i] + max1 + max2)
+        }
+    }
+
+    return res
+}
