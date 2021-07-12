edc65d4f73d34996b2db3acccd0fd23597b7af08
vivek mittal <viv.mttl@gmail.com>
2021-07-12 15:57:02 +0530

    add math/greatestCommonDivisor.go

---
+++ math/problems/greatestCommonDivisor.go
@@ -0,0 +1,14 @@
+
+// T(n) : O(log(n)), S(n) : O(1)
+func gcd(A int , B int )  (int) {
+
+    if A < B {
+        return gcd(B, A)
+    }
+
+    if B == 0 {
+        return A
+    }
+
+    return gcd(B, A % B)
+}
