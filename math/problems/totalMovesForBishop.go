20e9b52aa5ebf834a9470591688285310ee01875
vivek mittal <viv.mttl@gmail.com>
2021-07-14 17:43:54 +0530

    add math/totalMovesForBishop.go

---
+++ math/problems/totalMovesForBishop.go
@@ -0,0 +1,14 @@
+
+func min(a, b int) (int) {
+    if a < b {
+        return a
+    }
+    return b
+}
+
+// T(n) : O(1), S(n) : O(1)
+func solve(A int , B int )  (int) {
+
+    ud, dd, ld, rd := A - 1, 8 - A, B - 1, 8 - B
+    return min(ud, rd) + min(ud, ld) + min(dd, ld) + min(dd, rd)
+}
