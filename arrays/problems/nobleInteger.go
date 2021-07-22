a545d06aea9ee70bbefb904da692b9fb5744c369
vivek mittal <viv.mttl@gmail.com>
2021-07-22 18:24:55 +0530

    add array/noble-integer.go

---
+++ arrays/problems/nobleInteger.go
@@ -0,0 +1,22 @@
+import "sort"
+
+// T(n) : O(nlog(n)) , S(n) : O(1)
+func solve(A []int )  (int) {
+
+    sort.Slice(A, func(i, j int) bool {
+        return A[i] <= A[j]
+    })
+
+    n := len(A)
+    if A[n - 1] == 0 {
+        return 1
+    }
+
+    for i := 0; i < n - 1; i++ {
+        if A[i] == n - i - 1 && A[i] != A[i + 1] {
+            return 1
+        }
+    }
+
+    return -1
+}
