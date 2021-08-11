4d1e85d540fb2d398857f327f67a40c59996f864
vivek mittal <viv.mttl@gmail.com>
2021-08-11 13:12:35 +0530

    add binary_search/rotatedArray.go

---
+++ Binary Search/problems/rotatedArray.go
@@ -0,0 +1,17 @@
+
+// T(n) : O(log(n)), S(n) : O(1)
+func findMin(A []int ) (int) {
+    
+    n := len(A)
+    l, r := 0, n - 1
+    for l < r {
+        mid := l + (r - l) / 2
+        if A[mid] > A[n - 1] {
+            l = mid + 1
+        } else {
+            r = mid
+        }
+    }
+    
+    return A[l]
+}
