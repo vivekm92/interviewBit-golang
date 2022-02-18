48683689c65b303e93302d12b843cd33a5f5a7ae
vivekm <viv.mttl@gmail.com>
2022-02-19 00:43:14 +0530

    update

--- src/math/problems/fizzBuzz.go
+++ src/math/problems/fizzBuzz.go
@@ -1,25 +1,27 @@
+package math
+
 import "strconv"
 
 // T(n) : O(n), S(n) : O(n)
-func fizzBuzz(A int )  ([]string) {
+func fizzBuzz(A int) []string {
 
-    res := make([]string, 0)
-    for i := 1; i <= A; i++ {
-        var foo string = ""
-        if i % 3 == 0 {
-            foo += "Fizz"
-        } 
-        
-        if i % 5 == 0 {
-            foo += "Buzz"
-        }
-        
-        if foo == "" {
-            foo = strconv.Itoa(i)
-        }
+	res := make([]string, 0)
+	for i := 1; i <= A; i++ {
+		var foo string = ""
+		if i%3 == 0 {
+			foo += "Fizz"
+		}
 
-        res = append(res, foo)
-    }
+		if i%5 == 0 {
+			foo += "Buzz"
+		}
 
-    return res
+		if foo == "" {
+			foo = strconv.Itoa(i)
+		}
+
+		res = append(res, foo)
+	}
+
+	return res
 }
