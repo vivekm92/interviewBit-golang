f6600eb279b71f65a157ea5f70d929577e464a78
vivekm <viv.mttl@gmail.com>
2022-05-08 17:00:05 +0530

    update arrays/additionalPractice/arraySum.go

--- src/arrays/additionalPractice/arraySum.go
+++ src/arrays/additionalPractice/arraySum.go
@@ -7,7 +7,7 @@ package additionalPractice
 
   1) Two Pointer Approach
 
-	- Keep pointers at the end of each array
+	- Keep pointers at the end of each array.
 	- keep adding the values pointed at each array and decrease the pointer value.
 	- reverse the resultant array.
 
@@ -15,7 +15,12 @@ package additionalPractice
 
   1) Try to do the above problem without using any additional space.
 
-
+	- Use the longer input array as the resultant array.
+	- Save the sum in resultant array using two pointer approach.
+	- if carry is present then append the resultant array with carry.
+	- right shift all the elemnets by 1.
+	- set carry as the first element of the array.
+	- return the array.
 
 */
 
@@ -53,6 +58,91 @@ func addArrays(A []int, B []int) []int {
 	return arr
 }
 
+// T(n) : O(n), S(n) : O(1)
+func addArraysFollowUp(A []int, B []int) []int {
+
+	nA, nB := len(A)-1, len(B)-1
+	res, iC := A, nA
+	if nB > nA {
+		res = B
+		iC = nB
+	}
+
+	iA, iB, carry := nA, nB, 0
+	for iA >= 0 || iB >= 0 {
+		valA, valB := 0, 0
+		if iA >= 0 {
+			valA = A[iA]
+			iA--
+		}
+
+		if iB >= 0 {
+			valB = B[iB]
+			iB--
+		}
+
+		sum := valA + valB + carry
+		res[iC] = sum % 10
+		iC--
+		carry = sum / 10
+	}
+
+	if carry != 0 {
+		return append([]int{carry}, res...)
+	}
+
+	return res
+}
+
+// T(n) : O(n), S(n) : O(1)
+func addArraysFollowUp1(A []int, B []int) []int {
+
+	nA, nB := len(A)-1, len(B)-1
+	res, iC := A, nA
+	if nB > nA {
+		res = B
+		iC = nB
+	}
+
+	iA, iB, carry := nA, nB, 0
+	for iA >= 0 || iB >= 0 {
+		valA, valB := 0, 0
+		if iA >= 0 {
+			valA = A[iA]
+			iA--
+		}
+
+		if iB >= 0 {
+			valB = B[iB]
+			iB--
+		}
+
+		sum := valA + valB + carry
+		res[iC] = sum % 10
+		iC--
+		carry = sum / 10
+	}
+
+	if carry != 0 {
+		res = append(res, carry)
+		nC := len(res)
+		for i := nC - 1; i > 0; i-- {
+			res[i] = res[i-1]
+		}
+		res[0] = carry
+	}
+
+	return res
+}
+
 func SolveArraySum(A []int, B []int) []int {
 	return addArrays(A, B)
+}
+
+func SolveArraySumFollowUp(A []int, B []int) []int {
+	return addArraysFollowUp(A, B)
+}
+
+func SolveArraySumFollowUp1(A []int, B []int) []int {
+	return addArraysFollowUp1(A, B)
 }
