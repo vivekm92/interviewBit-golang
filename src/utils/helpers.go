52546a8849513df72ad0074fb865c5941d5d9fe7
vivekm92 <viv.mttl@gmail.com>
2022-08-20 21:25:21 +0530

    add helpers.go

--- src/utils/helpers.go
+++ src/utils/helpers.go
@@ -1,1 +1,21 @@
 package utils
+
+func MaxOfInts(v ...int) int {
+	x := v[0]
+	for _, y := range v {
+		if y > x {
+			x = y
+		}
+	}
+	return x
+}
+
+func MinOfInts(v ...int) int {
+	x := v[0]
+	for _, y := range v {
+		if y < x {
+			x = y
+		}
+	}
+	return x
+}
---
+++ src/utils/helpers_test.go
@@ -0,0 +1,11 @@
+package utils_test
+
+import "testing"
+
+func TestMaxOfInts(t *testing.T) {
+	// t.Run()
+}
+
+func TestMinOfInts(t *testing.T) {
+
+}
