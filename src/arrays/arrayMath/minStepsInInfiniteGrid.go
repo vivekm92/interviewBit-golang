4896511974774dbe87e28a0892b9507c97061f62
vivekm92 <viv.mttl@gmail.com>
2023-07-11 02:57:54 +0530

    add test case : arrayMath/minStepsInInfiniteGrid.go

--- src/arrays/arrayMath/minStepsInInfiniteGrid.go
+++ src/arrays/arrayMath/minStepsInInfiniteGrid.go
@@ -2,6 +2,15 @@ package arrayMath
 
 import "math"
 
+/*
+  Problem : https://www.interviewbit.com/problems/min-steps-in-infinite-grid/
+
+  Solution :
+    - All the points need to be covered in order ( in which they are given ).
+	- since we can move in diagonal as well in cartesian plane, when we move from (x,y) to (a,b)
+	- minimum steps to move from (x,y) --> (a,b) would be max(abs(a-x), abs(b-y))
+*/
+
 // T(n) : O(n), S(n) : O(1)
 func CoverPoints(A []int, B []int) int {
 
--- src/arrays/arrayMath/minStepsInInfiniteGrid_test.go
+++ src/arrays/arrayMath/minStepsInInfiniteGrid_test.go
@@ -1,7 +1,30 @@
-package arrayMath_test
+package arrayMath
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type coverPointsTestCase struct {
+	A        []int
+	B        []int
+	Expected int
+}
+
+var coverPointsTestCases = []coverPointsTestCase{
+	{
+		[]int{0, 1, 1},
+		[]int{0, 1, 2},
+		2,
+	},
+}
 
 func TestCoverPoints(t *testing.T) {
 
+	for idx, test := range coverPointsTestCases {
+		if output := CoverPoints(test.A, test.B); output != test.Expected {
+			fmt.Println(test.A, test.B, test.Expected, output)
+			t.Errorf("Failed %v testCase \nGOT : %v \nEXPECTED : %v\n", idx, output, test.Expected)
+		}
+	}
 }
