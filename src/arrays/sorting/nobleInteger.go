28d37dd32a2a8292a730e3f0a4753cb034bfdab1
vivekm92 <viv.mttl@gmail.com>
2023-07-16 02:52:45 +0530

    udpate array sort : noble integer

--- src/arrays/sorting/nobleInteger.go
+++ src/arrays/sorting/nobleInteger.go
@@ -22,3 +22,23 @@ func NobleIntegers(A []int) int {
 
 	return -1
 }
+
+func NobleInteger(A []int) int {
+
+	sort.Slice(A, func(i, j int) bool {
+		return A[i] < A[j]
+	})
+
+	n := len(A)
+	for i := 0; i < n; i++ {
+		if i == n-1 && A[i] == 0 {
+			return 1
+		} else if i < n-1 && A[i] != A[i+1] {
+			if A[i] == n-1-i {
+				return 1
+			}
+		}
+	}
+
+	return -1
+}
