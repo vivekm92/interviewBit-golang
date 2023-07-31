14845dac349516f67953b75eaf34fe740920e7f4
vivekm92 <viv.mttl@gmail.com>
2023-08-01 01:15:05 +0530

    add bsearch : smallerOrEqualElementsTestCase

---
+++ src/binarySearch/problems/smallerOrEqualElements.go
@@ -0,0 +1,19 @@
+package bsearch
+
+// T(n) : O(logn), S(n): O(1)
+func SmallerOrEqualElements(A []int, B int) int {
+
+	n := len(A)
+	l, r := 0, n
+
+	for l < r {
+		mid := l + (r-l)/2
+		if A[mid] <= B {
+			l = mid + 1
+		} else {
+			r = mid
+		}
+	}
+
+	return l
+}
