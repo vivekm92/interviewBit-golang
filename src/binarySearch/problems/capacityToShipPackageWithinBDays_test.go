8187bb13e6cdc67cf586a4ec6961545b74102b55
vivekm92 <viv.mttl@gmail.com>
2023-08-02 03:02:17 +0530

    update bsearch : capacityToShipPackageWithinBDays.go

--- src/binarySearch/problems/capacityToShipPackageWithinBDays_test.go
+++ src/binarySearch/problems/capacityToShipPackageWithinBDays_test.go
@@ -11,7 +11,7 @@ type findLeastCapacityTestCase struct {
 	Expected int
 }
 
-var findLeastCapacityTestCases = []findElementCountTestCase{
+var findLeastCapacityTestCases = []findLeastCapacityTestCase{
 	{[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 5, 15},
 	{[]int{3, 2, 2, 4, 1, 4}, 3, 6},
 }
