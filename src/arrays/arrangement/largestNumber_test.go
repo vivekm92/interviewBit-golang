2cc13dd9cf8b238249d2a47bb6064426ec4a2ddf
vivekm92 <viv.mttl@gmail.com>
2023-08-06 16:11:38 +0530

    arrays arrangement : update largestNumber.go

--- src/arrays/arrangement/largestNumber_test.go
+++ src/arrays/arrangement/largestNumber_test.go
@@ -1,7 +1,28 @@
-package arrangement_test
+package arrangement
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+type largestNumberTestCase struct {
+	A        []int
+	Expected string
+}
+
+var largestNumberTestCases = []largestNumberTestCase{
+	{
+		[]int{3, 30, 34, 5, 9},
+		"9534330",
+	},
+}
 
 func TestLargestNumber(t *testing.T) {
 
+	for idx, test := range largestNumberTestCases {
+		if output := LargestNumber(test.A); test.Expected != output {
+			fmt.Println(test.A, test.Expected, output)
+			t.Errorf("Failed %v testCase \nGOT : %v \nEXPECTED : %v\n", idx, output, test.Expected)
+		}
+	}
 }
