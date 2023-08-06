fb8f38b448bf3b5069b1e7ac15022dde23606e90
vivekm92 <viv.mttl@gmail.com>
2023-08-07 03:22:35 +0530

    array arrangement : update rotateMatrix.go

--- src/arrays/arrangement/rotateMatrix.go
+++ src/arrays/arrangement/rotateMatrix.go
@@ -1,1 +1,24 @@
 package arrangement
+
+// T(n) : O(n^2), S(n): O(1)
+func Rotate(A [][]int) [][]int {
+
+	n := len(A)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n/2; j++ {
+			t := A[j][i]
+			A[j][i] = A[n-1-j][i]
+			A[n-1-j][i] = t
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			t := A[i][j]
+			A[i][j] = A[j][i]
+			A[j][i] = t
+		}
+	}
+
+	return A
+}
---
+++ src/arrays/arrangement/rotateMatrix_test.go
@@ -0,0 +1,57 @@
+package arrangement
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+type rotateMatrixTestCase struct {
+	A        [][]int
+	Expected [][]int
+}
+
+var rotateMatrixTestCases = []rotateMatrixTestCase{
+	{
+		[][]int{
+			{1, 2, 3, 4},
+			{5, 6, 7, 8},
+			{9, 10, 11, 12},
+			{13, 14, 15, 16},
+		},
+		[][]int{
+			{13, 9, 5, 1},
+			{14, 10, 6, 2},
+			{15, 11, 7, 3},
+			{16, 12, 8, 4},
+		},
+	},
+	{
+		[][]int{
+			{1, 2},
+			{3, 4},
+		},
+		[][]int{
+			{3, 1},
+			{4, 2},
+		},
+	},
+	{
+		[][]int{
+			{1},
+		},
+		[][]int{
+			{1},
+		},
+	},
+}
+
+func TestRotate(t *testing.T) {
+
+	for idx, test := range rotateMatrixTestCases {
+		if output := Rotate(test.A); !reflect.DeepEqual(test.Expected, output) {
+			fmt.Println(test.A, test.Expected, output)
+			t.Errorf("Failed %v testCase \nGOT : %v \nEXPECTED : %v\n", idx, output, test.Expected)
+		}
+	}
+}
