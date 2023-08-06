84073ac4b908f958f6a50d8cb868f20e601025c3
vivekm92 <viv.mttl@gmail.com>
2023-08-06 14:59:06 +0530

    arrays spacerecycle : add firstMissingInteger.go

---
+++ src/arrays/spaceRecycle/firstMissingInteger.go
@@ -0,0 +1,20 @@
+package spacerecycle
+
+func FirstMissingPositive(A []int) int {
+
+	n := len(A)
+	arr := make([]bool, n+1)
+	for i := 0; i < n; i++ {
+		if A[i] > 0 && A[i] <= n {
+			arr[A[i]] = true
+		}
+	}
+
+	for i := 1; i <= n; i++ {
+		if !arr[i] {
+			return i
+		}
+	}
+
+	return n + 1
+}
