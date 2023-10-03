907371282765fc577db3850f58be711f76bfb141
vivekm92 <viv.mttl@gmail.com>
2023-10-04 02:51:28 +0530

    twoPointers : add mergeTwoSortedLists2.go

---
+++ src/twoPointers/problems/mergeTwoSortedLists2.go
@@ -0,0 +1,27 @@
+package twoPointers
+
+// T(n): O(n), S(n): O(n)
+func Merge(A []int, B []int) []int {
+
+	n, m := len(A), len(B)
+	c := make([]int, 0)
+	i, j := 0, 0
+	for i < n || j < m {
+		if i < n && j < m {
+			if A[i] < B[j] {
+				c = append(c, A[i])
+				i++
+			} else {
+				c = append(c, B[j])
+				j++
+			}
+		} else if i < n {
+			c = append(c, A[i])
+			i++
+		} else {
+			c = append(c, B[j])
+			j++
+		}
+	}
+	return c
+}
