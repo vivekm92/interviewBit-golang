20abac8b7590667152c555af64982082aebfe7c4
vivekm92 <viv.mttl@gmail.com>
2023-10-29 17:27:19 +0530

    strings : add zigzagString.go

---
+++ src/strings/problems/zigzagString.go
@@ -0,0 +1,35 @@
+package stringProblems
+
+/*
+	problem : https://www.interviewbit.com/problems/zigzag-string/
+*/
+
+// T(n) : O(n), S(n) : O(n)
+func ZigZagString(A string, B int) string {
+
+	n := len(A)
+	if B == 1 || B >= n {
+		return A
+	}
+
+	idx, step := 0, 1
+	t := make([][]rune, B)
+	for _, v := range A {
+		t[idx] = append(t[idx], v)
+		if idx == 0 {
+			step = 1
+		} else if idx == B-1 {
+			step = -1
+		}
+		idx += step
+	}
+
+	res := ""
+	for _, v1 := range t {
+		for _, v2 := range v1 {
+			res += string(v2)
+		}
+	}
+
+	return res
+}
