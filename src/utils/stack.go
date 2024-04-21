87401f864d39aa6bb7adbc6b7bfdad7d08067b4a
vivekm92 <viv.mttl@gmail.com>
2024-04-21 06:59:20 +0530

    add stack implementation

--- src/utils/stack.go
+++ src/utils/stack.go
@@ -1,23 +1,80 @@
 package utils
 
-type Stack []interface{}
+import (
+	"errors"
+	"sync"
+)
 
-func (s *Stack) IsEmpty() bool {
-	return len(*s) == 0
+type Stack[T any] struct {
+	data []T
+	head int
+	lock sync.Mutex
 }
 
-func (s *Stack) Push(data interface{}) {
-	*s = append(*s, data)
+func NewStack[T any](capacity int) *Stack[T] {
+	return &Stack[T]{
+		data: make([]T, capacity),
+	}
 }
 
-func (s *Stack) Pop() (interface{}, bool) {
-	if s.IsEmpty() {
-		return "", false
+func (s *Stack[T]) Count() int {
+	s.lock.Lock()
+	count := s.head
+	s.lock.Unlock()
+	return count
+}
+
+func (s *Stack[T]) Capacity() int {
+	return len(s.data)
+}
+
+func (s *Stack[T]) Push(val T) error {
+
+	s.lock.Lock()
+	if len(s.data) == s.head {
+		s.lock.Unlock()
+		return errors.New("the buffer is full and cannot take more data")
 	}
 
-	idx := len(*s) - 1
-	element := (*s)[idx]
-	*s = (*s)[:idx]
+	s.data[s.head] = val
+	s.head += 1
+	s.lock.Unlock()
 
-	return element, true
+	return nil
+}
+
+func (s *Stack[T]) Peek() (T, bool) {
+	var found bool
+	var v T
+	s.lock.Lock()
+	if s.head > 0 {
+		idx := s.head - 1
+		v = s.data[idx]
+		found = true
+	}
+
+	s.lock.Unlock()
+	return v, found
+}
+
+func (s *Stack[T]) Pop() (T, bool) {
+
+	var found bool
+	var v T
+	var blank T
+	s.lock.Lock()
+	if s.head > 0 {
+		idx := s.head - 1
+		v = s.data[idx]
+		found = true
+		s.data[idx] = blank
+		s.head -= 1
+	}
+
+	s.lock.Unlock()
+	return v, found
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return s.head == 0
 }
