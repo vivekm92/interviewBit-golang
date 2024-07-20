115987a795ad3d968718fb17690d2f669d31614a
Vivek Mittal <vivek.mittal@redBus.com>
2024-07-21 00:16:17 +0530

    revise : rotateMatrix.go

--- src/arrays/problems/rotateMatrix.go
+++ src/arrays/problems/rotateMatrix.go
@@ -6,9 +6,7 @@ func Rotate(A [][]int) [][]int {
 	n := len(A)
 	for i := 0; i < n; i++ {
 		for j := 0; j < n/2; j++ {
-			t := A[j][i]
-			A[j][i] = A[n-1-j][i]
-			A[n-1-j][i] = t
+			A[j][i], A[n-1-j][i] = A[n-1-j][i], A[j][i]
 		}
 	}
 
